cfa712d7c1f973bd560dedb738d8965fd7e3121d
FGD192 <fgd280704@gmail.com>
2020-07-09 14:55:36 +0600

    lesson6 menu

---
+++ app/src/main/java/es/fgd192/lesson6/MainActivity.kt
@@ -0,0 +1,30 @@
+package es.fgd192.lesson6
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var BAct1 = findViewById<Button>(R.id.act1)
+        BAct1.setOnClickListener{
+            var intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    }
